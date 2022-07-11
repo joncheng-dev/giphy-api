dd7f2271cdfa22faa74f42f4fa426d99edf490d9
Jonathan Cheng <jcheng461@gmail.com>
2022-07-10 23:00:29 -0700

    add user interface buttons to search for gif by trending and random -- via API call

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import "./css/styles.css";
 
 // User Interface Logic
 $(document).ready(function () {
-  // Search for gif by keyword
+  // Search for gifs by keyword
   $("#searchKeyword").click(function () {
     const keyword = $("#keyword").val();
     $("#keyword").val("");
 
     let request = new XMLHttpRequest();
-    // Check for accuracy.
     const url = `https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${process.env.API_KEY}`;
 
     request.onreadystatechange = function () {
@@ -24,18 +23,48 @@ $(document).ready(function () {
     request.open("GET", url, true);
     request.send();
   });
-  // Search for gif by trending
-  $("#").click(function () {
+  // Search for gifs by trending
+  $("#searchTrending").click(function () {
     // Trending
-  });
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}`;
 
-  // Still need spaces in between
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+  });
+  // Search for gifs by random
+  $("#searchRandom").click(function () {
+    // Random
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+  });
+  // Parses data and selects images to display to HTML
   function getElements(response) {
     let htmlDisplay = "";
-    for (let i = 0; i < response.data.length; i++) {
-      htmlDisplay += `<a href="${response.data[i].images.original.url}"><img src="${response.data[i].images.original.url}" alt="${response.data[i].title}"></a>`;
+    if (response.data.length > 1) {
+      for (let i = 0; i < response.data.length; i++) {
+        htmlDisplay += `<a href="${response.data[i].images.original.url}"><img src="${response.data[i].images.original.url}" alt="${response.data[i].title}"></a>`;
+      }
+    } else {
+      htmlDisplay += `<a href="${response.data.images.original.url}"><img src="${response.data.images.original.url}" alt="${response.data.title}"></a>`;
     }
     $(".showResults").html(`${htmlDisplay}`);
-    //  end of html display ---- alt="${response.data[i]}"
   }
 });
